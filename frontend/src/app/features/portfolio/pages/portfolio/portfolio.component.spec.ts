8ad6a8ea9782eff4b06f2f2fcdba6dcb02db64ba
Juan Mario Sosa Romo <juanmario@ciencias.unam.mx>
2026-07-13 21:29:33 -0600

    fix(test): provideRouter for PortfolioComponent to fix tests

--- frontend/src/app/features/portfolio/pages/portfolio/portfolio.component.spec.ts
+++ frontend/src/app/features/portfolio/pages/portfolio/portfolio.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { PortfolioComponent } from './portfolio.component';
 
@@ -8,7 +9,8 @@ describe('PortfolioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PortfolioComponent]
+      imports: [PortfolioComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
