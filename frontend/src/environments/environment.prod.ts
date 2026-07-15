222598deb5604c92ea130ce875b43602d2c749f2
Juan Mario Sosa Romo <juanmario@ciencias.unam.mx>
2026-07-15 17:06:01 -0600

    fix: centralize version display and bump to v0.2.0

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: `${window.location.protocol}//${window.location.hostname}`
+  apiUrl: `${window.location.protocol}//${window.location.hostname}`,
+  appVersion: 'v0.2.0'
 };
