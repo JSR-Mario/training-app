1930453f226d3ed208fb3e42268bc008dbcf8e7f
Juan Mario Sosa Romo <juanmario@ciencias.unam.mx>
2026-07-24 18:42:53 -0600

    feat: live exercise reorder list preview and unit accent styling

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: `${window.location.protocol}//${window.location.hostname}`,
-  appVersion: 'v0.27.4'
+  appVersion: 'v0.28.0'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  appVersion: 'v0.27.4',
+  appVersion: 'v0.28.0',
   apiUrl: 'http://localhost:8080/api/v1'
 };
