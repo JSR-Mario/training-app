5d958b51e389c33beaf2a801069afd8e8489b62b
Juan Mario Sosa Romo <juanmario@ciencias.unam.mx>
2026-07-25 20:26:21 -0600

    fix: resolve duplicate api/v1 paths in frontend HTTP requests

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  appVersion: 'v0.29.3',
-  apiUrl: 'http://localhost:8080/api/v1'
+  appVersion: 'v0.29.4',
+  apiUrl: 'http://localhost:8080'
 };
